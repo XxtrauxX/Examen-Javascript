671ca69f27b2a634dd4c99e1cd46a92ee5013d93
oscar <aaronjones2429@gmail.com>
2025-01-23 22:21:44 -0500

    subiendo los ultimos cambios al examen

---
+++ modulo-js/registro.js
@@ -0,0 +1,9 @@
+const formu = document.querySelector('#signup')
+
+
+formu.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+})
